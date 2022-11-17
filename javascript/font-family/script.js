924da0e7bede02fff11385bbb0fbdd07f0937d39
HelenMayer <you@example.com>
2022-11-17 10:25:00 +0200

    add new folders

--- javascript/font-family/script.js
+++ javascript/font-family/script.js
@@ -5,3 +5,8 @@ function ButtonClick() {
     document.getElementById("text").style.fontFamily=array[index%5]
     document.getElementById("fontFamily").textContent="Fontti on nyt: "+array[index%5]
 }
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
